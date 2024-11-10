366a0aa842ee2e5442c8ab6bdd196fd25d82c035
Eu, de paula <gfpaula.cic@uesc.br>
2024-11-09 22:30:56 -0300

    addes file of interval search

---
+++ praticaTresTiposAbstratos/firstinterval.c
@@ -0,0 +1,17 @@
+/*
+programam with 3 user interactions 
+    1.add and interval of numbers
+        1.1 use queue or linked list
+    2.verify some number in any of the intervals added
+    3.exit
+  instructions:
+    if user insert 1:
+        read 2 numbers, 
+        the bigger be the 2 on the linked list
+    if user want to check
+        read a number
+        run the linked list and verify if the number is in some interval
+        if yes: put interval, order of the linked list and number
+    if user exit
+        free memory and exit  
+*/
