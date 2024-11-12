ae1ad1c512cf1f3c86a715bdc378918c64cfb9f0
Eu, de paula <gfpaula.cic@uesc.br>
2024-11-12 19:15:05 -0300

    added the file of the second question using ADS

---
+++ praticaTresTiposAbstratos/calendar.c
@@ -0,0 +1,13 @@
+/*
+Calendar:
+    manage up to 100 contacts 
+                        must add and edit informations like:
+                                                            email
+                                                            phone
+                                                            name
+                                                            address
+                        operations:
+                                    Add contact
+                                    show contactssearch for contact
+                                                        disclaimer: use various methods
+*/
