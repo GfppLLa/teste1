af047466cbcbcc2f70b5c398bf407bd62960bc3f
Eu, de paula <gfpaula.cic@uesc.br>
2024-11-20 23:34:43 -0300

    uptades in calendar
    the calendar function implementation updades include: almoast all the functions finished

--- praticaTresTiposAbstratos/calendar/calenda_list.c
+++ praticaTresTiposAbstratos/calendar/calenda_list.c
@@ -23,7 +23,6 @@ Calendar:
 */
 #include<stdio.h>
 #include<stdlib.h>
-#include<conio.h>
 #define MAX_LOOP 4
 #define MAX_SIZE 100
 typedef struct List{
@@ -34,7 +33,22 @@ typedef struct List{
     struct LIST*next;
 
 } node;
-
+/**
+ * 
+ * createNode - put the content in the list array
+ * 
+ * description:
+ * 
+ * 
+ * 
+ * parameters:
+ * @head: a pointer to pointer of the addres of the head element of the list
+ * 
+ * @content: a pointer to pointer of an 2d array containing the data
+ * 
+ * return:
+ * the addres of the new node already alloced and the data insert
+ */
 node**crateNode(node**head, char**content)//only called bay the insert node
 {
     node* newList;
@@ -86,13 +100,158 @@ node**crateNode(node**head, char**content)//only called bay the insert node
             }
 
             free(toAuxLoop);
-            return newList;
+            
         }
+    }
+     //todo: remove from here the insertion of the content in the list node
+    //maybe not
+ //todo: remember to change te function to void
+//todo again: change the passing of addres to the insert function, 
+return head;
+}
+/**
+ * 
+ * insertNode - put the node in the arraylist
+ * 
+ * description:
+ * 
+ * 
+ * 
+ * parameters:
+ * @head: a pointer to pointer of the addres of the head element of the list
+ * 
+ * @content: a pointer to pointer of an 2d array containing the data
+ * 
+ * return:
+ * the head adddres already changed
+ */
+node**insertNode(node**head, char**content)//to insert at end
+{
+    //todo: transfer to here the insertion of the content in the list node
+   //not todo: the previously one
+  //19/11: not used, by now
+  node *newList=crateNode(&head, &content);
+  if(newList!=NULL)
+  {
+    (*newList).next=*head;
+    *head=newList;
+  }
+return newList;
+}
+/**
+ * 
+ * findNode - return or not the address to a certain string diven
+ * description:
+ * ind and return the address of the node using the element given as key, the **content houses the 
+ * element in the index mathing its type, the int instructor denote the type 
+ * 
+ * 
+ * parameters:
+ * @
+ * 
+ * @
+ * 
+ * return: the address
+ * 
+ */
+node**findNode(node** head, char**content, int instructor)//called in finding node != and in removing node
+{
+    node* runAux=head;
+    char ;
+    while((*runAux).next !=NULL)
+    {
+        switch (instructor)
+        {
+        case 1://nome
+            if( strcmp((*runAux).name,content[instructor])==0)
+            {
+                return runAux;
+            }else{
+                runAux=(*runAux).next;
+            }  
+            break;
+        case 2://phone
+            if( strcmp((*runAux).phone,content[instructor])==0)
+            {
+                return runAux;
+            }else{
+                runAux=(*runAux).next;
+            }  
+            break;
+        case 3://mail
+            if( strcmp((*runAux).mail,content[instructor])==0)
+            {
+                return runAux;
+            }else{
+                runAux=(*runAux).next;
+            }  
+            break;
+        case 4://address
+            if( strcmp((*runAux).address,content[instructor])==0)
+            {
+                return runAux;
+            }else{
+                runAux=(*runAux).next;
+            }  
+            break;
+        }
+    }
+    return NULL;
+}
+/**
+ * 
+ * removeNode - remove a node from the list
+ * 
+ * description: receives a adddres and removes its from the memory
+ * 
+ * 
+ * 
+ * parameters:
+ * @head: address of the fisrt element
+ * 
+ * return:
+ * 
+ */
+removeNode(node**head, node*targetNode)
+{
+    node *auxOnde;
+    //first verify if the target node is the first
+    if (*head == targetNode)
+    {
+        node* tempOne=*head;
+        *head= (*head)->next;//not sure why use two dereferenciation
+        free(tempOne);
+        return;
+    }
+    //to do if the target node is not the first
+    node*currentNode=*head;
+    while((*currentNode).next != NULL && (*currentNode).next!= targetNode)
+    {
+        currentNode=(*currentNode).next;//exit when find the node we're looking for or reach the last list
+    }
+
+    if((*currentNode).next==(*targetNode).next)
+    {
+        (*currentNode).next=(*targetNode).next;//the current node receives the node that the target pointed
+        free(targetNode);                     //and remove the target
     }
 
 }
-node**insertNode(node**head, char*content);//to insert at end
-node**findNode(node** head, char*content, int instructor);//called in finding node and in removing node
-node**removeNode(node**head, char*content, int intructor);
+/**
+ * 
+ * findNode - 
+ * 
+ * description:
+ * 
+ * 
+ * 
+ * parameters:
+ * @
+ * 
+ * @
+ * 
+ * return:
+ * 
+ */
 void freeNode(node**head);//only receives de node and remove it
 
