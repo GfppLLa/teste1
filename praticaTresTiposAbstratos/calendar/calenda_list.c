41b8b54fdb94cbd8df94904d035b7ca9e8a67222
Eu, de paula <gfpaula.cic@uesc.br>
2024-11-22 23:27:33 -0300

    updates in calendar file and implementation

--- praticaTresTiposAbstratos/calendar/calenda_list.c
+++ praticaTresTiposAbstratos/calendar/calenda_list.c
@@ -1,7 +1,7 @@
 /*implementation file*/
 /*
 Calendar:
-    manage up to 100 contacts 
+    manage up to 100 contacts
                         must add and edit informations like:
                                                             email
                                                             phone
@@ -11,7 +11,7 @@ Calendar:
                                     Add contact
                                                 the terminal read the informations from the user
                                     show contacts
-                                                show all the contacts 
+                                                show all the contacts
                                     search for contact
                                                         disclaimer: use various methods
                                     remove contact(to be decided)
@@ -21,251 +21,270 @@ Calendar:
     main->
 
 */
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define MAX_LOOP 4
 #define MAX_SIZE 100
-typedef struct List{
-    char*name;
-    char*phone;
-    char*mail;
-    char*address;
-    struct List*next;
+typedef struct List
+{
+    char *name;
+    char *phone;
+    char *mail;
+    char *address;
+    struct List *next;
 
 } node;
 /**
- * 
+ *
  * createNode - put the content in the list array
- * 
+ *
  * description:
- * 
- * 
- * 
+ *
+ *
+ *
  * parameters:
  * @head: a pointer to pointer of the addres of the head element of the list
- * 
+ *
  * @content: a pointer to pointer of an 2d array containing the data
- * 
+ *
  * return:
  * the addres of the new node already alloced and the data insert
  */
-node**crateNode(node**head, char**content)//only called bay the insert node
+node **crateNode(node **head, char **content) // only called bay the insert node
 {
-    node* newList;
-    newList=(node*)calloc(1, sizeof(node));
-    if(newList==NULL){
-        //do something
-    }else{
-        //do other thing
-        //for loop to run the 2diomensions array
-        int j=0;
-        for(int i=0;i<MAX_LOOP; i++)
+    node *newList;
+    newList = (node *)calloc(1, sizeof(node));
+    if (newList == NULL)
+    {
+        // do something
+    }
+    else
+    {
+        // do other thing
+        // for loop to run the 2diomensions array
+        int j = 0;
+        for (int i = 0; i < MAX_LOOP; i++)
         {
-            //second for loop to run tho
-            char*toAuxLoop;
-            toAuxLoop=(char*)calloc(MAX_SIZE, (sizeof(char)));
+            // second for loop to run tho
+            char *toAuxLoop;
+            toAuxLoop = (char *)calloc(MAX_SIZE, (sizeof(char)));
             do
-            {//do something
-                toAuxLoop[j]=content[i][j];
+            { // do something
+                toAuxLoop[j] = content[i][j];
                 j++;
-            }while(content[i][j]!='\0');//using while loop saves the need to do a if check only to find the null character
-            toAuxLoop[j]='\0';//not very clear why but surely with some purpose
-            for(int k=0; ;k++)
+            } while (content[i][j] != '\0'); // using while loop saves the need to do a if check only to find the null character
+            toAuxLoop[j] = '\0'; // not very clear why but surely with some purpose
+            for (int k = 0;; k++)
             {
-                if(i==0)//the first field in the LL
+                if (i == 0) // the first field in the LL
                 {
-                    (*newList).name[k]=toAuxLoop[k];
-                }else {
-                    if(i==1)
+                    (*newList).name[k] = toAuxLoop[k];
+                }
+                else
+                {
+                    if (i == 1)
                     {
-                        (*newList).phone[k]=toAuxLoop[k];
-                    }else{
-                        if(i==2)
+                        (*newList).phone[k] = toAuxLoop[k];
+                    }
+                    else
+                    {
+                        if (i == 2)
                         {
-                            (*newList).mail[k]=toAuxLoop[k];
-                        }else{
-                            if(i==3)
+                            (*newList).mail[k] = toAuxLoop[k];
+                        }
+                        else
+                        {
+                            if (i == 3)
                             {
-                                (*newList).address[k]=toAuxLoop[k];
+                                (*newList).address[k] = toAuxLoop[k];
                             }
                         }
                     }
                 }
-                if(toAuxLoop[k]=='\0')
+                if (toAuxLoop[k] == '\0')
                 {
                     break;
-                }else{
-                    continue;//the cat syndrome
+                }
+                else
+                {
+                    continue; // the cat syndrome
                 }
             }
 
             free(toAuxLoop);
-            
         }
     }
-     //todo: remove from here the insertion of the content in the list node
-    //maybe not
- //todo: remember to change te function to void
-//todo again: change the passing of addres to the insert function, 
-return head;
+    // todo: remove from here the insertion of the content in the list node
+    // maybe not
+    // todo: remember to change te function to void
+    // todo again: change the passing of addres to the insert function,
+    return head;
 }
 /**
- * 
+ *
  * insertNode - put the node in the arraylist
- * 
+ *
  * description:
- * 
- * 
- * 
+ *
+ *
+ *
  * parameters:
  * @head: a pointer to pointer of the addres of the head element of the list
- * 
+ *
  * @content: a pointer to pointer of an 2d array containing the data
- * 
+ *
  * return:
  * the head adddres already changed
  */
-node**insertNode(node**head, char**content)//to insert at end
+node **insertNode(node **head, char **content) // to insert at end
 {
-    //todo: transfer to here the insertion of the content in the list node
-   //not todo: the previously one
-  //19/11: not used, by now
-  node *newList=crateNode(&head, &content);
-  if(newList!=NULL)
-  {
-    (*newList).next=*head;
-    *head=newList;
-  }
-return newList;
+    // todo: transfer to here the insertion of the content in the list node
+    // not todo: the previously one
+    // 19/11: not used, by now
+    node *newList = crateNode(&head, &content);
+    if (newList != NULL)
+    {
+        (*newList).next = *head;
+        *head = newList;
+    }
+    return newList;
 }
 /**
- * 
+ *
  * findNode - return or not the address to a certain string diven
  * description:
- * ind and return the address of the node using the element given as key, the **content houses the 
- * element in the index mathing its type, the int instructor denote the type 
- * 
- * 
+ * ind and return the address of the node using the element given as key, the **content houses the
+ * element in the index mathing its type, the int instructor denote the type
+ *
+ *
  * parameters:
  * @
- * 
+ *
  * @
- * 
+ *
  * return: the address
- * 
+ *
  */
-node**findNode(node** head, char**content, int instructor)//called in finding node != and in removing node
+node **findNode(node **head, char **content, int instructor) // called in finding node != and in removing node
 {
-    node* runAux=head;
+    node *runAux = head;
 
-    while((*runAux).next !=NULL)
+    while ((*runAux).next != NULL)
     {
         switch (instructor)
         {
-        case 1://nome
-            if( strcmp((*runAux).name,content[instructor])==0)
+        case 1: // nome
+            if (strcmp((*runAux).name, content[instructor]) == 0)
             {
                 return runAux;
-            }else{
-                runAux=(*runAux).next;
-            }  
+            }
+            else
+            {
+                runAux = (*runAux).next;
+            }
             break;
-        case 2://phone
-            if( strcmp((*runAux).phone,content[instructor])==0)
+        case 2: // phone
+            if (strcmp((*runAux).phone, content[instructor]) == 0)
             {
                 return runAux;
-            }else{
-                runAux=(*runAux).next;
-            }  
+            }
+            else
+            {
+                runAux = (*runAux).next;
+            }
             break;
-        case 3://mail
-            if( strcmp((*runAux).mail,content[instructor])==0)
+        case 3: // mail
+            if (strcmp((*runAux).mail, content[instructor]) == 0)
             {
                 return runAux;
-            }else{
-                runAux=(*runAux).next;
-            }  
+            }
+            else
+            {
+                runAux = (*runAux).next;
+            }
             break;
-        case 4://address
-            if( strcmp((*runAux).address,content[instructor])==0)
+        case 4: // address
+            if (strcmp((*runAux).address, content[instructor]) == 0)
             {
                 return runAux;
-            }else{
-                runAux=(*runAux).next;
-            }  
+            }
+            else
+            {
+                runAux = (*runAux).next;
+            }
             break;
         }
     }
     return NULL;
 }
 /**
- * 
+ *
  * removeNode - remove a node from the list
- * 
+ *
  * description: receives a adddres and removes its from the memory
- * 
- * 
- * 
+ *
+ *
+ *
  * parameters:
  * @head: address of the fisrt element
- * 
+ *
  * return:
- * 
+ *
  */
-removeNode(node**head, node*targetNode)
+removeNode(node **head, node *targetNode)
 {
     node *auxOnde;
-    //first verify if the target node is the first
+    // first verify if the target node is the first
     if (*head == targetNode)
     {
-        node* tempOne=*head;
-        *head= (*head)->next;//not sure why use two dereferenciation
+        node *tempOne = *head; //
+        *head = (*head)->next; // not sure why use two dereferenciation
         free(tempOne);
         return;
     }
-    //to do if the target node is not the first
-    node*currentNode=*head;
-    while((*currentNode).next != NULL && (*currentNode).next!= targetNode)
+    // to do if the target node is not the first
+    node *currentNode = *head;
+    while ((*currentNode).next != NULL && (*currentNode).next != targetNode)
     {
-        currentNode=(*currentNode).next;//exit when find the node we're looking for or reach the last list
+        currentNode = (*currentNode).next; // exit when find the node we're looking for or reach the last list
     }
 
-    if((*currentNode).next==(*targetNode).next)
+    if ((*currentNode).next == (*targetNode).next)
     {
-        (*currentNode).next=(*targetNode).next;//the current node receives the node that the target pointed
-        free(targetNode);                     //and remove the target
+        (*currentNode).next = (*targetNode).next; // the current node receives the node that the target pointed
+        free(targetNode);                         // and remove the target
     }
-
 }
 /**
- * 
- * findNode - 
- * 
+ *
+ * findNode -
+ *
  * description:
- * 
- * 
- * 
+ *
+ *
+ *
  * parameters:
  * @
- * 
+ *
  * @
- * 
+ *
  * return:
- * 
+ *
  */
-void freeNode(node**head)//only receives de node and remove it
+void freeNode(node **head) // only receives de node and remove it
 {
-    node *auxOne=*head;
-    while ((*auxOne).next!=NULL)
+    node *auxOne = *head;
+    while ((*auxOne).next != NULL)
     {
-        if((*auxOne).next==NULL)
+        if ((*auxOne).next == NULL)
         {
             free(auxOne);
-        }else{
-            node*proto=auxOne;//only created to serve as aux and to be freed
-            auxOne=(*auxOne).next;
+        }
+        else
+        {
+            node *proto = auxOne; // only created to serve as aux and to be freed
+            auxOne = (*auxOne).next;
             free(proto);
         }
     }
-    
 }
--- praticaTresTiposAbstratos/calendar/secondcalendar.c
+++ praticaTresTiposAbstratos/calendar/secondcalendar.c
@@ -1,1 +1,137 @@
 /*main file*/
+#include<stdlib.h>
+#include<stdio.h>
+#include "calendar_list.h"
+/*
+here :
+        menu
+        Calendar:
+    manage up to 100 contacts 
+                        must add and edit informations like:
+                                                            email
+                                                            phone
+                                                            name
+                                                            address
+                        operations:
+                                    Add contact
+                                                the terminal read the informations from the user
+                                    show contacts
+                                                show all the contacts 
+                                    search for contact
+                                                        disclaimer: use various methods
+                                    remove contact(to be decided)
+                                    exit
+    structure->
+             ->createNode
+    main->
+*/
+/*
+menu function must run indefinitely
+                                    ->read a number from terminal
+                                    ->compare the number to the options
+                                    ->if number, call function correspondently
+                                            1->add: stack tree
+                                            =function read returns the readings
+                                                2->show all
+                                                    3-> search for a contact; stack tree
+                                                    =function to read something
+                                                    return to the search function
+                                                        4->remove contact
+                                                        =function read will return the char to be compared
+
+*/  
+#define MAX_SIZE 100
+#define MIN_SIZE 1
+#define STRUCT_ELEMENTS 4
+#include <stdio.h>
+#include <stdlib.h>
+void clearBuffer()
+{
+    int charac;
+    while((charac=getchar())!='\n'&&charac!=EOF)
+    {
+
+    }
+}
+void removeNewLine(char*plaintext)
+{
+    //kinda explain itself
+    int i=0;
+    while (plaintext[i]!='\n')
+    {
+        if(plaintext[i]=='\n'){plaintext[i]='\0'; break;}
+        i++;
+    }
+}
+char*read()
+{
+ //using the max_size read the keyboard and the return it 
+    char *reading;   
+    reading=(char*)calloc(MAX_SIZE, sizeof(char));
+        if (reading==NULL)
+        {
+            exit(1);
+        }
+        while(1)
+        {
+            //read and only break if greater than min size
+            fscanf(stdin, "%[^\n]", reading);
+            clearBuffer;
+            //the logic to verify the size
+            size_t len=strlen(reading);
+            if(len>=MIN_SIZE&&len<MAX_SIZE-1)
+            {
+                //sair
+                removeNewLine(reading);
+                return reading;
+            }
+            printf("\n******insert maximum of 100 letters*****");
+        }
+}
+void operationD()//remove contact
+{
+
+}
+void operationC()//search contact
+{
+
+}
+void operationB()//show all elements
+{
+
+}
+void operationA(node**calendarlist)//add element on the list
+{
+      //create a 2d char array
+     //use the read function to fill the collunmns
+    //at he end sent it to the insertNode()
+    char**content;
+    
+    for(int i=0; i<STRUCT_ELEMENTS; i++)//run the lines
+    {
+
+    }
+
+}
+int main(int argc, char*argv[])
+{
+    node*calendarList;
+    while(1)
+    {
+        printf("\n--menu--\n");
+            while(1)
+            {
+                char operationToken=getche();
+                if(operationToken>='1'&&operationToken<='4'){
+                    if(operationToken=='1')operationA(&calendarList);
+                    if(operationToken=='2')operationB();
+                    if(operationToken=='3')operationC();
+                    if(operationToken=='4')operationD();
+                    break;
+                }
+                printf("\nbe aware of the options***\n");
+            }
+             //the outer loop to descide if get in the op menu again
+            //must ask if exit ou again
+    }
+}
